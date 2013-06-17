c6642e9c1b9204683f157fb1335a356d5e54dc98
Jonathan Cordeiro <Jonathan0Cordeiro@gmail.com>
2013-06-17 14:05:03 -0400

    Add .sh file

---
+++ sortphotos.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Written by: Jonathan Cordeiro
+# June 17th, 2013
+#
+# Run this script in the 'Camera Uploads' folder
+# of your Dropbox and it will sort through all
+# of your pictures and place them into directories
+# based on the year they were taken
+#
+# As of right now, this script only works with
+# pictures taken by iPhones because it is 
+# dependent on their naming conventions of
+# YEAR-MONTH-DAY.[file_extension]
+# i.e 2010-01-23.png
+#
+# This first version of the script only searches
+# for .jpg, .png, and .mov files, but other file
+# types can be supported by adding them to the list
+# on line 38
+
+# Make directories, supress warnings if they already exist
+
+mkdir 2010 2> /dev/null
+mkdir 2011 2> /dev/null
+mkdir 2012 2> /dev/null
+mkdir 2013 2> /dev/null
+
+
+# Move photos and videos into correct folders
+
+for DIR in 2010 2011 2012 2013
+do
+
+	echo -e "\nSorting through $DIR photos and videos"
+
+  # For each extension search through and mv the files into their new directory
+  for ext in jpg png mov
+  do
+      if [ $(ls $DIR*.$ext 2> /dev/null | wc -l) -ne 0 ]
+      then
+         echo " $(ls $DIR*.$ext 2> /dev/null | wc -l) .$ext files sorted for $DIR"
+         mv $DIR*.$ext $DIR 2> /dev/null
+      else
+         echo "No .$ext files to sort for $DIR"
+      fi 
+  done
+
+done
+
+echo -e "\nSorting completed\n"
+
